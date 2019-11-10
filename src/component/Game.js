b96aefd02f8851585ead91eab7bf11552b17ddfc
hini <heunw7@gmail.com>
2019-11-10 17:09:08 +0900

    fixed game.js

--- src/component/Game.js
+++ src/component/Game.js
@@ -9,7 +9,7 @@ class Game extends React.Component {
     componentDidMount(){
         /*game variable*/
         var num_size=10; // random number 범위
-        var num; // 출력할 숫자 변수 ( start에서의 변수만 저장됨 )
+        var num=[]; // 출력할 숫자 변수 ( start에서의 변수만 저장됨 )
 
         var color_list ={ // 랜덤으로 나올 색상
             red:0xc9382a,
@@ -20,9 +20,9 @@ class Game extends React.Component {
             pupple:0xa322d6,
             pink:0xe374cd
         };
-        var color; // 출력할 색상 변수 ( start에서의 변수만 저장됨 )
+        var color=[]; // 출력할 색상 변수 ( start에서의 변수만 저장됨 )
         
-        var level = 1; // 레벨
+        var level = 4; // 레벨
         var tryNumber=level; // 레벨에 따라 출력되는 숫자가 많아짐
 
         var startGameButton = document.getElementsByClassName('start_button')[0];
@@ -34,9 +34,6 @@ class Game extends React.Component {
         /*game variable*/
 
         /*general object of three.js*/
-
-        var objects = [];
-
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         scene.add(camera);
@@ -52,39 +49,48 @@ class Game extends React.Component {
 
         /*general object of three.js*/
 
-        function createText(kind){ // text 생성
-            var number;
+        function createText(kind, create_num){ // text 생성
+            console.log("text");
+
+            var message;
             var randomColor;
+            var size =80;
+            var posX =-40;
+            var posY =20;
+            var posZ =-100;
 
             if(kind === 'start'){ // 저장된 변수 출력
-                number = String(num);
-                randomColor = color;
-            } else{ // 랜덤 변수 출력
-                number = String(Math.floor(Math.random()*num_size <<0));
-                randomColor = randColor(color_list);
+                message = String(num[create_num]);
+                randomColor = color[create_num];
+            }else if(kind === 'first'){
+                message= "위쪽의"+""+"시작버튼을"+""+"눌러주세요!";   
+                randomColor = 0x000000;
+                size=25;
+                posX=-200;
+            }else if(kind==='answer'){ // 랜덤 변수 출력
+                message= "아래의"+""+"보기중"+""+"하나를"+""+"선택해주세요!";   
+                randomColor = 0xffffff;
+                size=23;
+                posX=-230;
             }
             
+            console.log(message, randomColor);
             var loader = new THREE.FontLoader();
 
             loader.load( 'fonts/dohyeon.typeface.json', function ( font ) {
-                var geometry = new THREE.TextGeometry( number, {
+                var geometry = new THREE.TextGeometry( message, {
                     font: font,
-                    size: 80,
+                    size: size,
                     height: 0,
-                    curveSegments: 12,
-                    bevelEnabled: true,
-                    bevelThickness: 1,
-                    bevelSize: 1,
-                    bevelOffset: 0,
-                    bevelSegments: 0
+                    curveSegments: 12
                     } );
                 var meshMaterial = new THREE.MeshBasicMaterial({
                     color: randomColor
                 });
                 text = new THREE.Mesh(geometry, meshMaterial);
-                text.position.x=-40;
-                text.position.y=20;
-                text.position.z=-100;
+                text.position.x=posX;
+                text.position.y=posY;
+                text.position.z=posZ;
                 scene.add(text);
             } );
         }
@@ -99,31 +105,11 @@ class Game extends React.Component {
             })
         }
 
-        function waitBackGround(){ // 기다리면서 배경 잠시 바꾸기
+        function answerWait(){ // 기다리면서 배경 잠시 바꾸기
+            console.log("wait");
             var canvas = document.getElementsByClassName('three')[0].children[0];
-            var i =1;
-            var count = setInterval(function(){
-                console.log(i++);
-            },1000)
-
-            return new Promise(resolve=>{
-                canvas.style="background-color:black";
-                setTimeout(function(){
-                    resolve(20);
-                    console.log("wait");
-                    canvas.style="background-color:transparent";
-                
-                    clearInterval(count);
-                },2000);
-            })
-        }
-
-        async function confuseText(){ // 헷갈리게 하기위해 5개의 랜덤 변수 출력
-            console.log('confuse');
-            for(var i=0; i<5; i++){
-                createText('confuse');
-                await removeText(1000);
-            }
+            canvas.style="background-color:black";
+            createText('answer');
         }
 
         function randColor(obj) { // 랜덤 색상을 리스트에서 뽑아주는 코드
@@ -141,20 +127,21 @@ class Game extends React.Component {
 
         async function startGame(){ // start에서의 숫자 : num, 색상 : color 에 저장
             console.log("start");
-            num = Math.floor(Math.random()*num_size <<0);
-            color = randColor(color_list);
+            for(var i=0; i<tryNumber; i++){
+                num.push(Math.floor(Math.random()*num_size << 0));
+                color.push(randColor(color_list));
+            }
+            console.log(num,color);
 
             startGameButton.style="display:none";
             resetGameButton.style="display:block";
 
+            await removeText(0);
             for(var i=0; i<tryNumber; i++){
-                createText('start');
-                await removeText(4000);
+                createText('start',i);
+                await removeText(2000);
             }
-            await waitBackGround();
-            await confuseText();
-            await waitBackGround();
-            await createText('start');
+            answerWait();
         }
 
         function animate() {
@@ -162,7 +149,10 @@ class Game extends React.Component {
             renderer.render( scene, camera );
         };
 
-        animate();
+        window.onload=function(){
+            createText('first');
+            animate();
+        }
     }
 
     render(){
@@ -173,10 +163,12 @@ class Game extends React.Component {
 					goto = {this.props.goto.bind(this)}
                 />
                 <div className="game_button">
-                    <button className="start_button">start</button>
-                    <button className="reset_button">reset</button>
+                    <button className="start_button">시작</button>
+                    <button className="reset_button">다시하기</button>
+                    <div className="instruct_text">다음의 숫자와 색깔을 기억하세요!</div>
                 </div>
                 <div className="three"></div>
+                <div className="example"></div>
             <script src="build/three.js" type="module"></script>
             <script src="src/loaders/FontLoader.js" type="module"></script>
             </div>
